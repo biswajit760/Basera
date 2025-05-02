f6f0cc5f0968f3294231b0d060d7b99dc67b5800
Biswajit Mahanty <155170520+biswajit760@users.noreply.github.com>
2025-05-02 10:21:46 +0530

    middleware changes

--- middleware.js
+++ middleware.js
@@ -22,7 +22,16 @@ module.exports.saveRedirectUrl = (req, res, next) => {
   next();
 };
 
+module.exports.isOwner = async(req, res, next) => {
+  let {id} = req.params;
+  let listing = await Listing.findById(id);
+  if(!listing.owner._id.equals(res.locals.currUser._id)){
+    req.flash("error", "You are not Owner for this listing");
+    return res.redirect(`/listings/${id}`);
+  }
 
+  next();
+}
 
 module.exports.validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
@@ -33,19 +42,6 @@ module.exports.validateListing = (req, res, next) => {
     next();
   }
 };
-
-module.exports.isOwner = async(req, res, next) => {
-  let {id} = req.params;
-  let listing = await Listing.findById(id);
-  if(!listing.owner.equals(res.locals.currUser._id)){
-    req.flash("error", "You are not Owner for this listing");
-    return res.redirect(`/listings/${id}`);
-  }
-
-  next();
-}
-
-
 
 
 module.exports.validateReview = (req, res, next) => {
@@ -68,4 +64,4 @@ module.exports.isReviewAuthor = async(req, res, next) => {
     return res.redirect(`/listings/${id}`);
   }
   next(); 
-}
+};
